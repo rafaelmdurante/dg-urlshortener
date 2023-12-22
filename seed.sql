88327082f330d9d6f08dbe822ab094f6782f91a1
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2023-12-22 22:36:08 +0000

    build: create database and seed

---
+++ seed.sql
@@ -0,0 +1,18 @@
+-- create table to store the shortened urls
+create table url (
+                     id serial primary key,
+                     url_code varchar(6) not null default '',
+                     target_url text not null,
+                     created_at timestamp not null default now()
+);
+
+-- add first ten rows so it is easier to know if it is encoding correctly
+insert into url (url_code, target_url, created_at) values ('1', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('2', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('3', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('4', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('5', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('6', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('7', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('8', 'inserted by seed', now());
+insert into url (url_code, target_url, created_at) values ('9', 'inserted by seed', now());
