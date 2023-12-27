f6130e566c28a3be9d8e449acb781cf832c7a218
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2023-12-27 18:24:02 +0000

    build: add live reload container with air

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -2,9 +2,14 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func SetRoutes(g *gin.Engine) {
+	g.GET("/health", func(ctx *gin.Context) {
+		ctx.Status(http.StatusOK)
+	})
+
 	g.POST("/", CreateShortURL)
 	g.GET("/:code", RedirectToTargetURL)
 }
