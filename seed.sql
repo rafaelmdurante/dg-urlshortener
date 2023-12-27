107511ff6c50534d6a8bd3435bd095d60d29f3d3
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2023-12-27 19:26:08 +0000

    build: rename database from short_url to url

--- seed.sql
+++ seed.sql
@@ -1,5 +1,5 @@
 -- create table to store the shortened urls
-create table short_url (
+create table url (
                      id serial primary key,
                      url_code varchar(6) not null default '',
                      target_url text not null,
@@ -7,12 +7,13 @@ create table short_url (
 );
 
 -- add first ten rows so it is easier to know if it is encoding correctly
-insert into short_url (url_code, target_url, created_at) values ('1', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('2', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('3', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('4', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('5', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('6', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('7', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('8', 'inserted by seed', now());
-insert into short_url (url_code, target_url, created_at) values ('9', 'inserted by seed', now());
+-- the first 9 rows' url_code are exactly the same as their ids
+insert into url (url_code, target_url, created_at)values ('1', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('2', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('3', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('4', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('5', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('6', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('7', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('8', 'inserted by seed script', now());
+insert into url (url_code, target_url, created_at)values ('9', 'inserted by seed script', now());
